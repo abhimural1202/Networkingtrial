ab45ca0a44434232727349b9309fc2aadf2467cf
Muralidharan Dinakaran <abhimuraliadhina@gmail.com>
2025-03-08 21:55:30 +0000

    final for record

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "demo-resources"
-    storage_account_name  = "networkingautomation"
-    container_name        = "terraformstate"
-    key                   = "terraform.tfstate"
-  }
-}
